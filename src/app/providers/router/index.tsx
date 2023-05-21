0de63fde3092799ac9f7a53d741488748ad75d4b
Alexander <sashafromlibertalia@gmail.com>
2023-05-21 23:28:46 +0300

    feat: added pages

--- src/app/providers/router/index.tsx
+++ src/app/providers/router/index.tsx
@@ -2,11 +2,21 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { lazy } from "react";
 
 const RootPageLazy = lazy(() => import("@pages/root"));
+const ReportPageLazy = lazy(() => import("@pages/reports/[id]"));
+const Page404Lazy = lazy(() => import("@pages/404"));
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootPageLazy />,
+  },
+  {
+    path: "/reports/:id",
+    element: <ReportPageLazy />,
+  },
+  {
+    path: "*",
+    element: <Page404Lazy />,
   },
 ]);
 
---
+++ src/pages/reports/[id]/index.tsx
@@ -0,0 +1,11 @@
+import { AppLayout } from "@widgets";
+
+const ReportPage = () => {
+  return (
+    <AppLayout>
+      Report page
+    </AppLayout>
+  );
+};
+
+export default ReportPage;
